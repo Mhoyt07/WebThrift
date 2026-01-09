bff9c5f62cf56499d6912ade53e111daef250490
MH <mc_hoyt@outlook.com>
2026-01-08 19:38:16 -0600

    added testing database functions

--- scripts/test.py
+++ scripts/test.py
@@ -12,8 +12,26 @@ clothes.insert_one({
     "ID_no": 0,
     "date_added": datetime.now(),
     "class": "Long Sleeve T-shirt",
-    "color": "Green",
     "size": "L",
     "image_path": "PruneGreenSweater.jpeg"
     
 })
+
+def item_insert(ID_no, class_name, size, image_path):
+    clothes.insert_one({
+        "ID_no": ID_no,
+        "date_added": datetime.now(),
+        "class": class_name,
+        "size": size,
+        "image_path": image_path
+    })
+    
+    
+    print(f"Item {ID_no} inserted successfully.")
+
+def item_query(ID_no):
+    item = clothes.find_one({"ID_no": ID_no})
+    if item:
+        print(f"Item found: {item}")
+    else:
+        print(f"No item found with ID_no: {ID_no}")
