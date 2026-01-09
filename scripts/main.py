23bc77a8c2fb600fb17f0668e5e51ba95971165e
MH <mc_hoyt@outlook.com>
2026-01-08 20:32:15 -0600

    added main.py which communicates with arduino to run pipeline and display results

---
+++ scripts/main.py
@@ -0,0 +1,41 @@
+import serial
+from pipeline import pipeline
+import time
+
+def main():
+    PORT = '/dev/cu.usbmodem211401'  # <- your ESP32-CAM USB serial device
+
+    # Configure serial connection
+    ser = serial.Serial(PORT, 115200, timeout=1)
+    ser.flush()
+
+    blink_time = 0.25 # seconds
+    def blink_led(num):
+        # Single blink for items detected
+            ser.write(f"led{num}_on\n".encode('utf-8'))
+            time.sleep(blink_time)
+            ser.write(f"led{num}_off\n".encode('utf-8'))
+            time.sleep(blink_time)
+
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8', errors='ignore').strip()
+            if line:
+                if line == "start":
+                    print("Starting pipeline...")
+                    items = pipeline()
+                    items = -1 if items == 0 else items
+            if items > 0:
+                # Single blink for items detected
+                blink_led(1)
+                items = 0
+            elif items == -1:
+                # Double blink for no items detected
+                blink_led(2)
+                blink_led(2)
+                items = 0
+
+    ser.close()
+            
+if __name__ == "__main__":
+    main()
