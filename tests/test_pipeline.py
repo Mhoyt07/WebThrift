6f011f6844099032171f80b5b27cef764db09c59
VarunyThePruney <varungolakoti@gmail.com>
2026-01-22 14:30:26 +0530

    added more items to test

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -5,12 +5,17 @@ from app.pipeline import pipeline
 @pytest.mark.parametrize(
     "image_path, test_predictions, expected_classes, expected_sizes",
     [
-        ("images/item003.jpeg", [{"class": "t-shirt", "size": "M", "image_path": "images/item003.jpeg"}], "t-shirt", "M"),
+        ("images/test_item001.jpeg", [{"class": "shirt", "size": "M", "image_path": "images/test_item001.jpeg"}], "shirt", "M"),
+        ("images/test_item002.jpeg", [{"class": "shirt", "size": "M", "image_path": "images/test_item002.jpeg"}], "shirt", "M"),
+        ("images/test_item003.jpeg", [{"class": "t-shirt", "size": "M", "image_path": "images/test_item003.jpeg"}], "t-shirt", "M"),
+        ("images/test_item004.jpeg", [{"class": "sweater", "size": "M", "image_path": "images/test_item004.jpeg"}], "sweater", "M"),
+        
     ]
 )
 def test_pipeline(test_db, image_path, expected_classes, expected_sizes, test_predictions):
     """
-    Tests pipeline function without using the camera with tuple data
+    Tests pipeline function without using the camera or model with tuple data
+    Verifies that it inserts properly into the database
     """
     count, insert_success = pipeline(
         image_name="test_image",
